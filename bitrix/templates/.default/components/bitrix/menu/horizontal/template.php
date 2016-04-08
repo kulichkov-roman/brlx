351450fddaa4e1bcef46019945f9d88eab910970
Roman Kulichkov <roman@kulichkov.pro>
2016-04-08 14:42:34 +0600

    Добавил многоуровневое меню http://local.prostosait.ru/workgroups/group/93/tasks/task/view/3728/

--- bitrix/templates/.default/components/bitrix/menu/horizontal/template.php
+++ bitrix/templates/.default/components/bitrix/menu/horizontal/template.php
@@ -1,58 +1,84 @@
 <?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?$this->setFrameMode(true);?>
 
-<?if($USER->isAdmin())
-{
-	echo "<pre>"; var_dump($arResult); echo "</pre>";
-}
-?>
-
-<?if (!empty($arResult)):?>
-<?$compound = $arParams["COMPOUND"] == "Y"? " compound pull-right":""?>
-<ul class="flex-menu top-menu hidden-sm hidden-xs <?=$compound?>">
-
 <?
-$previousLevel = 0;
-	$flagFirst = true;
-foreach($arResult as $arItem):?>
-
-	<?if ($previousLevel && $arItem["DEPTH_LEVEL"] < $previousLevel):?>
-		<?=str_repeat("</ul></li>", ($previousLevel - $arItem["DEPTH_LEVEL"]));?>
-	<?endif?>
-
-	<?$addFirst = "";
-		$addClass=array();
-	if ($flagFirst){
-		$addClass[] = 'first';
-		$flagFirst = false;
+if(!function_exists('buildTree')){
+	function buildTree(array $dataset)
+	{
+		$tree = array();
+		/* Most datasets in the wild are enumerative arrays and we need associative array
+		where the same ID used for addressing parents is used. We make associative
+		array on the fly */
+		$references = array();
+		foreach($dataset as $id => & $node)
+		{
+			// Add the node to our associative array using it's ID as key
+			$references[$node['ID']] = & $node;
+			// Add empty placeholder for children
+			$node['CHILDREN'] = array();
+			// It it's a root node, we add it directly to the tree
+			if (is_null($node['PARENT_ID']))
+			{
+				$tree[$node['ID']] = & $node;
+			}
+			else
+			{
+				// It was not a root node, add this node as a reference in the parent.
+				$references[$node['PARENT_ID']]['CHILDREN'][$node['ID']] = & $node;
+			}
+		}
+		return $tree;
 	}
-	if ($arItem["SELECTED"]){
-		$addClass[] = 'selected';
+}
+$arParentIds = array();
+foreach($arResult as $key => $arItem){
+	$arItem["ID"] = $key;
+	if($arItem["IS_PARENT"]){
+		$arParentIds[$arItem["DEPTH_LEVEL"]] = $key;
 	}
-	if (count($addClass)>0){
-		$addFirst = ' class="'.implode(" ",$addClass).'"';
+	if($arItem["DEPTH_LEVEL"]>1){
+		$arItem["PARENT_ID"] = $arParentIds[$arItem["DEPTH_LEVEL"] - 1];
 	}
-	?>
-
-	<li<?=$addFirst?>><a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
-
-	<?if (!$arItem["IS_PARENT"]):?>
-		</li>
-	<?else:?>
-		<ul>
-	<?endif;?>
-
-	<?$previousLevel = $arItem["DEPTH_LEVEL"];?>
-
-<?endforeach?>
-
-<?if ($previousLevel > 1)://close last item tags?>
-	<?=str_repeat("</ul></li>", ($previousLevel-1) );?>
-<?endif?>
-
-	<li class="other last" id="flex-menu-li">&nbsp;</li>
-			<div class="clearfix"></div>
-</ul>
-
-<?endif?>
-
+	$arResult[$key] = $arItem;
+}
+$arTreeResult = buildTree($arResult);
+?>
+<?if (!empty($arTreeResult)){?>
+	<??>
+	<ul class="flex-menu top-menu hidden-sm hidden-xs">
+		<?
+		$index = 0;
+		foreach($arTreeResult as $key => $arItem){?>
+			<li class="<?=$index == 0 ? 'first selected' : '';?><?=$arItem["CHILDREN"] ? 'main-menu__item_has_submenu': '';?>">
+				<a href="<?=$arItem["LINK"]?>"><?=$arItem["TEXT"]?></a>
+				<?if($arItem["CHILDREN"]){?>
+					<div class="main-menu__submenu">
+						<ul class="main-menu__list main-menu__list_level_1">
+							<?foreach($arItem["CHILDREN"] as $arChildrenChunkDL1){?>
+								<li class="main-menu__item <?=$arChildrenChunkDL1["CHILDREN"] ? 'main-menu__item_has_submenu' : '';?>">
+									<a href="<?=$arChildrenChunkDL1["LINK"]?>" class="main-menu__link"><?=$arChildrenChunkDL1['TEXT']?></a>
+									<?if($arChildrenChunkDL1["CHILDREN"]){?>
+										<ul class="main-menu__list main-menu__list_level_2">
+											<?foreach($arChildrenChunkDL1["CHILDREN"] as $arChildrenChunkDL2){?>
+												<li class="main-menu__item">
+                                                       <a href="<?=$arChildrenChunkDL2['LINK']?>" class="main-menu__link"><?=$arChildrenChunkDL2['TEXT']?></a>
+                                                   </li>
+											<?}?>
+										</ul>
+									<?}?>
+								</li>
+							<?}?>
+						</ul>
+					</div>
+				<?}?>
+			</li>
+			<?
+			$index++;
+		}
+		?>
+		<li class="other last" id="flex-menu-li">
+               &nbsp;
+           </li>
+           <div class="clearfix"></div>
+	</ul>
+<?}?>
